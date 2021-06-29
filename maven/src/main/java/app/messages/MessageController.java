d89d9a2fa1e8c07feab7edcc10055a2f2fd9aa36
bellwest <bellwest93@gmail.com>
2021-06-30 00:43:56 +0900

    Day 8 - MVC
    use Model, Thymeleaf

--- maven/src/main/java/app/messages/MessageController.java
+++ maven/src/main/java/app/messages/MessageController.java
@@ -2,12 +2,14 @@ package app.messages;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+
 @Controller
 @RequestMapping("/messages")
 public class MessageController {
     @GetMapping("/welcome")
-    public String welcome() {
-        return "Hello, Welcome to Spring Boot!";
+    public String welcome(Model model) {
+        model.addAttribute("message", "Hello, Welcome to Spring Boot!");
+        return "welcome";
     }
 }
